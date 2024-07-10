e5e9b2ebda76b479d9d5811716a77464db549432
Lian, Cody WJ <cody.wj.lian@pccw.com>
2024-07-10 11:28:50 +0800

    Initialize the project step 1

---
+++ core/src/main/java/com/lifemanager/core/LifeManagerCoreApplication.java
@@ -0,0 +1,11 @@
+package com.lifemanager.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeManagerCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LifeManagerCoreApplication.class,args);
+    }
+}
